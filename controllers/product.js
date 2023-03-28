3b423548945957b40017437855458c17356e7d5a
Nodirbek Tojiboev <nodirbek_tojiboev@mail.ru>
2023-03-28 11:50:58 +0500

    new stayle, better images

--- controllers/product.js
+++ controllers/product.js
@@ -1,19 +1,5 @@
 let prods = [
     {
-        "title": "Chicken Alfredo",
-        "description": "Creamy pasta dish with grilled chicken, parmesan cheese, and garlic.",
-        "price": 12.99,
-        "imageUrl": "https://www.budgetbytes.com/wp-content/uploads/2022/07/Chicken-Alfredo-bowl.jpg",
-        "category": "Pasta"
-    },
-    {
-        "title": "Vegetarian Lasagna",
-        "description": "Layered pasta with spinach, mushrooms, ricotta cheese, and tomato sauce.",
-        "price": 11.99,
-        "imageUrl": "https://www.budgetbytes.com/wp-content/uploads/2022/07/Chicken-Alfredo-bowl.jpg",
-        "category": "Pasta"
-    },
-    {
         "title": "Margherita Pizza",
         "description": "Classic pizza with tomato sauce, mozzarella cheese, and fresh basil.",
         "price": 9.99,
@@ -24,59 +10,30 @@ let prods = [
         "title": "Pepperoni Pizza",
         "description": "Pizza with tomato sauce, mozzarella cheese, and pepperoni slices.",
         "price": 11.99,
-        "imageUrl": "https://www.cookingchanneltv.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg",
+        "imageUrl": "https://www.dogtownpizza.com/wp-content/uploads/2020/01/picking-slice-of-pepperoni-pizza-picture-id1133727757.jpg",
         "category": "Pizza"
     },
     {
         "title": "BBQ Chicken Pizza",
         "description": "Pizza with BBQ sauce, grilled chicken, red onion, and mozzarella cheese.",
         "price": 13.99,
-        "imageUrl": "https://www.cookingchanneltv.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg",
+        "imageUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/28/1/FNM_040112-Copy-That-002_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382541346030.jpeg",
         "category": "Pizza"
     },
     {
-        "title": "Grilled Salmon",
-        "description": "Fresh grilled salmon served with roasted vegetables and lemon butter sauce.",
-        "price": 16.99,
-        "imageUrl": "https://cdn.hswstatic.com/gif/fish-chip-2.jpg",
-        "category": "Seafood"
-    },
-    {
-        "title": "Fish and Chips",
-        "description": "Crispy beer-battered fish served with french fries and tartar sauce.",
+        "title": "Steak",
+        "description": "Classic buff steak with mashed potatoes and green beans.",
         "price": 12.99,
-        "imageUrl": "https://cdn.hswstatic.com/gif/fish-chip-2.jpg",
-        "category": "Seafood"
+        "imageUrl": "https://cdn.shopify.com/s/files/1/0880/9148/products/buffalo_ribeye.jpg?v=1568687343",
+        "category": "Comfort Food"
     },
     {
-        "title": "Beef Stir Fry",
-        "description": "Beef and vegetable stir fry served with steamed rice.",
-        "price": 14.99,
-        "imageUrl": "https://cdn.hswstatic.com/gif/fish-chip-2.jpg",
-        "category": "Asian"
-    },
-    {
-        "title": "Shrimp Pad Thai",
-        "description": "Thai rice noodles stir-fried with shrimp, bean sprouts, and peanuts.",
-        "price": 13.99,
-        "imageUrl": "https://www.foodandwine.com/thmb/8YAIANQTZnGpVWj2XgY0dYH1V4I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/spicy-chicken-curry-FT-RECIPE0321-58f84fdf7b484e7f86894203eb7834e7.jpg",
-        "category": "Asian"
-    },
-    {
-        "title": "Chicken Curry",
-        "description": "Spicy chicken curry with potatoes and rice.",
+        "title": "Chicken Alfredo",
+        "description": "Creamy pasta dish with grilled chicken, parmesan cheese, and garlic.",
         "price": 12.99,
-        "imageUrl": "https://www.foodandwine.com/thmb/8YAIANQTZnGpVWj2XgY0dYH1V4I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/spicy-chicken-curry-FT-RECIPE0321-58f84fdf7b484e7f86894203eb7834e7.jpg",
-        "category": "Asian"
+        "imageUrl": "https://www.budgetbytes.com/wp-content/uploads/2022/07/Chicken-Alfredo-bowl.jpg",
+        "category": "Pasta"
     },
-    {
-        "title": "Caesar Salad",
-        "description": "Romaine lettuce with croutons, parmesan cheese, and Caesar dressing.",
-        "price": 8.99,
-        "imageUrl": "https://www.foodandwine.com/thmb/8YAIANQTZnGpVWj2XgY0dYH1V4I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/spicy-chicken-curry-FT-RECIPE0321-58f84fdf7b484e7f86894203eb7834e7.jpg",
-        "category": "Salads"
-    },
-
     {
         "title": "Greek Salad",
         "description": "Mixed greens with cucumber, tomato, feta cheese, and Kalamata olives.",
@@ -85,40 +42,74 @@ let prods = [
         "category": "Salads"
     },
     {
+        "title": "Grilled Salmon",
+        "description": "Fresh grilled salmon served with roasted vegetables and lemon butter sauce.",
+        "price": 16.99,
+        "imageUrl": "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/grilled-miso-salmon-with-rice-noodles-58a2bbb.jpg?quality=90&resize=500,454",
+        "category": "Seafood"
+    },
+    {
+        "title": "Fish and Chips",
+        "description": "Crispy beer-battered fish served with french fries and tartar sauce.",
+        "price": 12.99,
+        "imageUrl": "https://forkandtwist.com/wp-content/uploads/2021/04/IMG_0102-500x500.jpg",
+        "category": "Seafood"
+    }, {
+        "title": "Vegetarian Lasagna",
+        "description": "Layered pasta with spinach, mushrooms, ricotta cheese, and tomato sauce.",
+        "price": 11.99,
+        "imageUrl": "https://realfood.tesco.com/media/images/303-vegetarian-vegetarian-lasagna-LH-11d5f8ab-b7ac-4036-ba5a-9add6b882d51-0-1400x919.jpg",
+        "category": "Pasta"
+    },
+    {
+        "title": "Beef Stir Fry",
+        "description": "Beef and vegetable stir fry served with steamed rice.",
+        "price": 14.99,
+        "imageUrl": "https://realfood.tesco.com/media/images/Beef-stir-fry-mini-mini-db138180-c70b-45ff-9f70-644e4fb3ac48-0-1400x919.jpg",
+        "category": "Asian"
+    },
+    {
+        "title": "Chicken Curry",
+        "description": "Spicy chicken curry with potatoes and rice.",
+        "price": 12.99,
+        "imageUrl": "https://recipesofhome.com/wp-content/uploads/2020/03/chicken-curry-recipe.jpg",
+        "category": "Asian"
+    },
+    {
         "title": "Steak Fajitas",
         "description": "Sizzling steak with onions and bell peppers served with tortillas.",
         "price": 15.99,
-        "imageUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/23/0/BX0204_greek-salad_s4x3.jpg.rend.hgtvcom.616.462.suffix/1529943050536.jpeg",
+        "imageUrl": "https://images-gmi-pmc.edge-generalmills.com/66785bb0-5226-4d45-9e3e-7b55af83cf3c.jpg",
         "category": "Mexican"
     },
     {
         "title": "Beef Tacos",
         "description": "Tacos with seasoned ground beef, lettuce, tomato, and cheese.",
         "price": 10.99,
-        "imageUrl": "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Mexican-Steak-Fajitas_EXPS_THJJ21_14546_B02_10_6b.jpg",
+        "imageUrl": "https://images-gmi-pmc.edge-generalmills.com/e4c2b240-a47f-4ad2-9382-8ef1484d7ceb.jpg",
         "category": "Mexican"
+    },
+    {
+        "title": "Caesar Salad",
+        "description": "Romaine lettuce with croutons, parmesan cheese, and Caesar dressing.",
+        "price": 8.99,
+        "imageUrl": "https://foodhub.scene7.com/is/image/woolworthsltdprod/tuna-caesar-salad:Mobile-1300x1150",
+        "category": "Salads"
     },
     {
         "title": "Chicken Enchiladas",
         "description": "Enchiladas with shredded chicken, cheese, and red sauce.",
         "price": 12.99,
-        "imageUrl": "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Mexican-Steak-Fajitas_EXPS_THJJ21_14546_B02_10_6b.jpg",
+        "imageUrl": "https://assets.kraftfoods.com/recipe_images/opendeploy/121713_640x428.jpg",
         "category": "Mexican"
     },
     {
         "title": "Roasted Chicken",
         "description": "Herb-roasted chicken served with mashed potatoes and gravy.",
         "price": 14.99,
-        "imageUrl": "https://www.budgetbytes.com/wp-content/uploads/2021/09/Classic-Meatloaf-side.jpg",
+        "imageUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/12/3/0/FN_Lemon-Herb-Roasted-Chicken_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384541143648.jpeg",
         "category": "Comfort Food"
     },
-    {
-        "title": "Meatloaf",
-        "description": "Classic meatloaf with mashed potatoes and green beans.",
-        "price": 12.99,
-        "imageUrl": "https://www.budgetbytes.com/wp-content/uploads/2021/09/Classic-Meatloaf-side.jpg",
-        "category": "Comfort Food"
-    }
 ]
 
 exports.products = () => {
--- controllers/views.js
+++ controllers/views.js
@@ -2,22 +2,30 @@ const products = require('./product');
 const User = require('../models/user');
 
 exports.getHome = async (req, res, next) => {
-    let userId = req.params.userId;
-    let prods = products.products();
-    let user = await User.findOne({ userId: userId });
+    try {
+        let userId = req.params.userId;
+        let prods = products.products();
+        let user = await User.findOne({ userId: userId });
 
-    res.render('index',
-        { products: prods, categories: products.cats(), userId: userId, username: user.fullname, number: user.number });
+        res.render('index',
+            { products: prods, current: 'main', categories: products.cats(), userId: userId, username: user.fullname, number: user.number });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 exports.postHome = async (req, res, next) => {
-    let data = req.params.category.split('+');
-    let catName = data[0];
-    let userId = data[1];
-    // console.log(catName);
-    let prods = products.someProducts(catName)
-    let user = await User.findOne({ userId: userId });
+    try {
+        let data = req.params.category.split('+');
+        let catName = data[0];
+        let userId = data[1];
+        // console.log(catName);
+        let prods = products.someProducts(catName)
+        let user = await User.findOne({ userId: userId });
 
 
-    res.render('index', { products: prods, categories: products.cats(), userId: userId, username: user.fullname, number: user.number });
+        res.render('index', { products: prods, current: catName, categories: products.cats(), userId: userId, username: user.fullname, number: user.number });
+    } catch (error) {
+        console.log(error);
+    }
 }
